2189597e6453890963b9598c73da10cf2239ad93
Nurgul Amat <nurgulamat212@gmail.com>
2022-01-14 14:13:50 -0600

    start the project

--- js/Game.js
+++ js/Game.js
@@ -9,10 +9,24 @@ class Game{
         this.missed = 0;
         this.phrases = [];
         this.activePhrase = null;
-    };
+    }
+
     // Create phrases for use in game
     createPhrases() {
+        const phraseArray = ['no pain no gain',
+                            'practice makes pgorgress',
+                            'It is great to be alive',
+                            'good things take time',
+                            'life is short'];
 
+        return phraseArray;
     };
-}
 
+    // Start the game 
+    startGame() {
+        document.getElementById('overlay').style.display = 'none';
+    }
+     
+
+
+}
