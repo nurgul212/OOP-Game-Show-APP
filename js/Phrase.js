2a0d1b8d2563e9073be9aee5cf109b76d5e7aed4
Nurgul Amat <nurgulamat212@gmail.com>
2022-01-16 21:35:16 -0600

    progress

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,7 +5,17 @@
 // declare Phrase class
 class Phrase{
     constructor(phrase){
-        this.phrase = phrase;
+        this.phrase = phrase.toLowerCse();
         
+    }
+
+    // This method adds letter placeholders to the display when the game starts.
+    addPhraseToDIsplay(){
+
+    }
+
+    // When the player correctly guesses a letter, the empty box is replaced with matched letter
+    showMatchedLetter(){
+
     }
 }
